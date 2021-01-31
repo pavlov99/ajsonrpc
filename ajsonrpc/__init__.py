bad91d50d352e4e659fa3fd7b8956411c8735e9a
Kirill Pavlov <k@p99.io>
2021-01-31 17:32:28 +0000

    DOC: add comments about version update

--- ajsonrpc/__init__.py
+++ ajsonrpc/__init__.py
@@ -1,4 +1,4 @@
 from .dispatcher import Dispatcher
 from .manager import AsyncJSONRPCResponseManager
 
-__version__ = version = "0.1.1"
+__version__ = "0.0.0"  # replaced with release tag in GitHub action
